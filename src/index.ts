f27c948eb46d6ab40ec30ea9a0eb72a75682792f
AvinashMSangle <avinashMS.3119@proton.me>
2025-01-08 08:52:20 +0000

    show unique req ids for pinologg

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import {OpenAPIHono} from '@hono/zod-openapi'
+import { pinoLogger } from 'hono-pino'
 import { notFound, onError } from 'stoker/middlewares'
 
 const app = new OpenAPIHono()
 
+app.use(pinoLogger({
+  http:{
+    reqId:()=>crypto.randomUUID(),
+  }
+}))
 app.notFound(notFound)
 app.onError(onError)
 
 app.get('/', (c) => {
+  c.var.logger.warn('This is a warning')
   return c.text('Hello Hono!')
 })
 
